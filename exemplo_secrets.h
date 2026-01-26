0a0d3a8df7440e6ae9e85921acff78dda1e4f1f0
edsoncepedi <edson.alves@cepedi.org.br>
2026-01-26 12:11:00 -0300

    Update exemplo_secrets.h

--- exemplo_secrets.h
+++ exemplo_secrets.h
@@ -15,4 +15,8 @@
 #define POSTO_ID 1
 #define USER_MQTT "posto_" STR(POSTO_ID)
 #define MQTT_TOPIC "rastreio_nfc/esp32/posto_" STR(POSTO_ID) "/dispositivo"
+#define IP_TOPIC "rastreio/esp32/posto_" STR(POSTO_ID) "/ip"
+#define MAC_TOPIC "rastreio/esp32/posto_" STR(POSTO_ID) "/mac"
+#define SISTEMA_TOPIC "rastreio/esp32/posto_" STR(POSTO_ID) "/sistema"
+
 //Deve-se criar o arquivo secrets.h preenchendo as definições. Esse arquivo é necessário para compilar o projeto.
